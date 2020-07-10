6b47434c0a643eda41023cf22f4eb9e9e3ddcd7d
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-10 18:51:29 -0500

    Build own directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ProductComponent } from './components/product.component';
+import { ProductComponent } from './components/product/product.component';
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
-import { ExponentialPipe } from './exponential.pipe';
+import { CartComponent } from './components/cart/cart.component';
+import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { HighlightDirective } from './directive/highlight/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, CartComponent, ExponentialPipe],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    CartComponent,
+    ExponentialPipe,
+    HighlightDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
