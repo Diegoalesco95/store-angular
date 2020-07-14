95c9e27b70fe961df4843c48abcb3937b03294ad
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-14 14:08:30 -0500

    Http Client Service

--- src/app/components/product/components/products/products.component.ts
+++ src/app/components/product/components/products/products.component.ts
@@ -14,11 +14,17 @@ export class ProductsComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
 
   ngOnInit() {
-    this.products = this.productsService.getAllProducts();
+    this.fetchProducts();
   }
 
   clickProduct(id: number) {
     console.log('Product');
     console.log(id);
+  }
+
+  fetchProducts() {
+    this.productsService.getAllProducts().subscribe((products) => {
+      this.products = products;
+    });
   }
 }
