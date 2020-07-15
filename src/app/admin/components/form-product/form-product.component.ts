d19dc57c93672e234835757b895926db732fd8e9
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-14 21:16:43 -0500

    Form Styles

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/services/products/products.service';
 
 @Component({
   selector: 'app-form-product',
@@ -9,11 +11,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormProductComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
+  ) {
     this.buildForm();
   }
 
   ngOnInit(): void {}
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe((newProduct) => {
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
 
   private buildForm() {
     this.form = this.formBuilder.group({
