fde85ffc2b1d798e5d121d396adbac830765af12
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-14 19:06:10 -0500

    Inventory List & details

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,6 +4,7 @@ import { NavComponent } from './components/nav/nav.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { ListProductsComponent } from './components/list-products/list-products.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
       {
         path: 'list',
         component: ListProductsComponent,
+      },
+      {
+        path: 'products',
+        component: ProductListComponent,
       },
     ],
   },
---
+++ src/app/admin/components/product-list/product-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/core/services/products/products.service';
+import { Product } from 'src/app/core/models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+  displayedColumns: string[] = ['id', 'title', 'price', 'actions'];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    this.productsService.getAllProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
+
+  deleteProduct(id: string): void {
+    this.productsService.deleteProduct(id).subscribe((response) => {
+      console.log('[Status product delete]: ', response);
+      if (response) {
+        const index = this.products.findIndex((product) => product.id === id);
+        this.products.splice(index, 1);
+        this.products = [...this.products];
+      }
+    });
+  }
+}
