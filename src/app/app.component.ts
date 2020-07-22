b81193e08f094588b5921702433904558aadbc1b
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-22 18:08:39 -0500

    SSR care

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 declare var gtag;
 
@@ -10,14 +11,16 @@ declare var gtag;
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private router: Router) {
-    const navEndEvents$ = this.router.events.pipe(
-      filter((event) => event instanceof NavigationEnd)
-    );
-    navEndEvents$.subscribe((event: NavigationEnd) => {
-      gtag('config', '240624695', {
-        page_path: event.urlAfterRedirects,
+  constructor(private router: Router, @Inject(DOCUMENT) private doc: Document) {
+    if (isPlatformBrowser(this.doc)) {
+      const navEndEvents$ = this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd)
+      );
+      navEndEvents$.subscribe((event: NavigationEnd) => {
+        gtag('config', '240624695', {
+          page_path: event.urlAfterRedirects,
+        });
       });
-    });
+    }
   }
 }
