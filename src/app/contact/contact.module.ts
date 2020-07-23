23cdaade14d48f28faf5ea5a43d4204e842440d4
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-23 08:55:37 -0500

    Change Detection

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
-import { ContactComponent } from './components/contact/contact.component';
 import { SharedModule } from '@shared/shared.module';
+import { LayoutComponent } from './components/layout/layout.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [ContactComponent],
+  declarations: [LayoutComponent, ListComponent],
   imports: [CommonModule, SharedModule, ContactRoutingModule],
 })
 export class ContactModule {}
