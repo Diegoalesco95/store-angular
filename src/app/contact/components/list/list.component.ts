9a655766a84df39049448e5fb7bd0438a5ddb2a4
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-07-23 09:27:58 -0500

    Preparing Change Detection Module

--- src/app/contact/components/list/list.component.ts
+++ src/app/contact/components/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { EmployeeData } from '@core/models/employee.model';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -7,8 +9,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListComponent implements OnInit {
   @Input() title: string;
+  @Input() data: EmployeeData[] = [];
+
+  label: string;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  addItem() {
+    this.data.push({
+      label: this.label,
+      num: 30,
+    });
+    this.label = '';
+  }
 }
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ContactRoutingModule } from './contact-routing.module';
 import { SharedModule } from '@shared/shared.module';
+import { MaterialModule } from '@material/material.module';
 import { LayoutComponent } from './components/layout/layout.component';
 import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [LayoutComponent, ListComponent],
-  imports: [CommonModule, SharedModule, ContactRoutingModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    ContactRoutingModule,
+    MaterialModule,
+    FormsModule,
+  ],
 })
 export class ContactModule {}
